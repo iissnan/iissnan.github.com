6682ad58df8c3f6c5842ee2593f3ef63893b68f9
iissnan <iissnan@gmail.com>
2014-01-26 08:09:28 -0800

    Simple Menu

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,6 +1,25 @@
 "user strict";
 
 angular.module("vi.controllers")
+    .controller("MenuController", function ($scope, $location, $route) {
+        var menuItems = {
+            "profile": "/",
+            "social": "social",
+            "lab": "lab",
+            "reading": "reading",
+            "equip": "equip"
+        };
+
+        $scope.go = function (path, event) {
+            if (menuItems[path]) {
+                $location.path(menuItems[path]);
+            } else{
+                $location.path("/");
+            }
+
+            event.preventDefault();
+        };
+    })
     .controller("ProfileController", function ($scope) {
         return function () {};
     })
