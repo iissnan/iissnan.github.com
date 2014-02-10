b592fb3e70d1d9057dc55cccb02b165867fb95e0
iissnan <iissnan@gmail.com>
2014-02-10 14:44:25 +0800

    Remove require.js

--- app/scripts/bootstrap.js
+++ app/scripts/bootstrap.js
@@ -1,90 +1,34 @@
-(function () {
-    require.config({
-        baseUrl: "../",
-        paths: {
-            "jquery": "bower_components/jquery/jquery",
-            "angular": "bower_components/angular/angular",
-            "angular.route": "bower_components/angular-route/angular-route",
-            "modules": "scripts/modules",
-            "controllers": "scripts/controllers",
-            "directives": "scripts/directives",
-            "services": "scripts/services",
-            "filters": "scripts/filters"
-        },
-        shim: {
-            "angular": {
-                deps: ["jquery"],
-                exports: "angular"
-            },
-            "angular.route": {
-                deps: ["angular"]
-            },
-            "controllers": {
-                deps: ["modules"]
-            },
-            "directives": {
-                deps: ["modules"]
-            },
-            "services": {
-                deps: ["modules"]
-            },
-            "filters": {
-                deps: ["modules"]
-            }
-        }
-    });
 
-    require([
-        "jquery",
-        "angular",
-        "angular.route",
-        "modules",
-        "controllers",
-        "directives",
-        "services",
-        "filters"
-    ], function($, angular) {
-        var APP_NAME = "iissnan";
 
-        angular.module(APP_NAME, [
-            "ngRoute",
-            "vi.controllers",
-            "vi.directives",
-            "vi.services",
-            "vi.filters"
-        ])
-            .config(function ($routeProvider, $locationProvider) {
-                $routeProvider
-                    .when("/", {
-                        controller: "ProfileController", 
-                        templateUrl: "/views/profile.html"
-                    })
-                    .when("/lab", {
-                        controller: "LabController", 
-                        templateUrl: "/views/lab.html"
-                    })
-                    .when("/read", {
-                        controller: "ReadController", 
-                        templateUrl: "/views/read.html"
-                    })
-                    .when("/equip", {
-                        controller: "EquipController", 
-                        templateUrl: "/views/equip.html"
-                    })
-                    .otherwise({
-                        redirecTo: "/"
-                    });
-
-                $locationProvider.hashPrefix("!");
+mainModule.config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when("/", {
+                controller: "ProfileController",
+                templateUrl: "/views/profile.html"
             })
-            .run(function () {
-                $(".loading").hide();
+            .when("/lab", {
+                controller: "LabController",
+                templateUrl: "/views/lab.html"
+            })
+            .when("/read", {
+                controller: "ReadController",
+                templateUrl: "/views/read.html"
+            })
+            .when("/equip", {
+                controller: "EquipController",
+                templateUrl: "/views/equip.html"
+            })
+            .otherwise({
+                redirectTo: "/"
             });
 
-        // Bootstrap the application.
-        $(document).ready(function () {
-            angular.bootstrap(document, [APP_NAME]);
-        });
+        $locationProvider.hashPrefix("!");
+    })
+    .run(function () {
+        $(".loading").hide();
     });
 
-}());
+// Bootstrap the application.
+$(document).ready(function () {
+    angular.bootstrap(document, [APP_NAME]);
+});
--- app/scripts/modules.js
+++ app/scripts/modules.js
@@ -1,8 +1,17 @@
-"use strict"
+"use strict";
 
-define(["angular"], function () {
-    angular.module("vi.controllers", []);                                                                                     
-    angular.module("vi.directives", []);
-    angular.module("vi.services", []);
-    angular.module("vi.filters", []);
-});
+angular.module("vi.controllers", []);
+angular.module("vi.directives", []);
+angular.module("vi.services", []);
+angular.module("vi.filters", []);
+
+var APP_NAME = "iissnan";
+var mainModule = angular.module(APP_NAME, [
+    "ngRoute",
+    "vi.controllers",
+    "vi.directives",
+    "vi.services",
+    "vi.filters"
+]);
+
+
